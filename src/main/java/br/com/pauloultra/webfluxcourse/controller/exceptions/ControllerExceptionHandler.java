dd91c44a52efecbb1aa404e07494e5f27066970d
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-12-12 11:03:54 -0300

    Exception Handler - Validation Error

--- src/main/java/br/com/pauloultra/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/pauloultra/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package br.com.pauloultra.webfluxcourse.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
@@ -26,6 +28,18 @@ public class ControllerExceptionHandler {
                                  .path(request.getPath().toString())
                                  .build()
                  ));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(WebExchangeBindException ex, ServerHttpRequest request) {
+        ValidationError error = new ValidationError(
+                now(), request.getPath().toString(), BAD_REQUEST.value(), "Validation error",
+                "Error on validation attributes"
+        );
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(Mono.just(error));
     }
 
     private String verifyDupKey(String message) {
--- src/main/java/br/com/pauloultra/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/pauloultra/webfluxcourse/model/request/UserRequest.java
@@ -1,6 +1,17 @@
 package br.com.pauloultra.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+        @Size(min = 3, max = 50, message = "must be between and 50 characters")
+        @NotBlank(message = "must not be null or empty")
         String name,
+
+        @Email(message = "invalid email")
         String email,
+
+        @Size(min = 3, max = 20, message = "must be between and 20 characters")
+        @NotBlank(message = "must not be null or empty")
         String password) {}
