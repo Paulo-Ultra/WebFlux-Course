2ca6e97762e56c287707d7e1a0120c55e7451d45
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-12-13 08:05:15 -0300

    Exception Handler - Custom annotation

--- src/main/java/br/com/pauloultra/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/pauloultra/webfluxcourse/model/request/UserRequest.java
@@ -1,17 +1,21 @@
 package br.com.pauloultra.webfluxcourse.model.request;
 
+import br.com.pauloultra.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
 
+        @TrimString
         @Email(message = "invalid email")
         String email,
 
+        @TrimString
         @Size(min = 3, max = 20, message = "must be between and 20 characters")
         @NotBlank(message = "must not be null or empty")
         String password) {}
