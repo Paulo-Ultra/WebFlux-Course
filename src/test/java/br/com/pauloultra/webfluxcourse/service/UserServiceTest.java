e6c9478faba1bc29ca79c6f4519dc16aa119f8c6
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-12-16 14:26:31 -0300

    Unit test to save User Class

---
+++ src/test/java/br/com/pauloultra/webfluxcourse/service/UserServiceTest.java
@@ -0,0 +1,51 @@
+package br.com.pauloultra.webfluxcourse.service;
+
+import br.com.pauloultra.webfluxcourse.entity.User;
+import br.com.pauloultra.webfluxcourse.mapper.UserMapper;
+import br.com.pauloultra.webfluxcourse.model.request.UserRequest;
+import br.com.pauloultra.webfluxcourse.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("Paulo", "paulo@mail.com","123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any())).thenReturn(entity);
+        when(userRepository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = userService.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(userRepository, times(1)).save(any(User.class));
+    }
+}
