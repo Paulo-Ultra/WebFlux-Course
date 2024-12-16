d5c5674f8d4fc1ea538d235af61d0541f61ab9b1
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-12-16 14:46:08 -0300

    Unit test findById - User Class

--- src/test/java/br/com/pauloultra/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/pauloultra/webfluxcourse/service/UserServiceTest.java
@@ -16,11 +16,14 @@ import reactor.test.StepVerifier;
 import java.util.Objects;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    public static final User USER = User.builder().build();
 
     @Mock
     private UserRepository userRepository;
@@ -32,20 +35,37 @@ class UserServiceTest {
     private UserService userService;
 
     @Test
-    void save() {
+    void testSave() {
         UserRequest request = new UserRequest("Paulo", "paulo@mail.com","123");
         User entity = User.builder().build();
 
         when(mapper.toEntity(any())).thenReturn(entity);
-        when(userRepository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+        when(userRepository.save(any(User.class))).thenReturn(Mono.just(USER));
 
         Mono<User> result = userService.save(request);
 
         StepVerifier.create(result)
-                .expectNextMatches(Objects::nonNull)
+                .expectNextMatches(user -> user.getClass() == User.class)
                 .expectComplete()
                 .verify();
 
         Mockito.verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void testFindById() {
+        when(userRepository.findById(anyString())).thenReturn(Mono.just(User.builder()
+                    .id("1234")
+                .build()));
+
+        Mono<User> result = userService.findById("123");
+
+        StepVerifier.create(result)
+//                .expectNextMatches(user -> user.getClass() == User.class && user.getId() == "1234")
+                .expectNextMatches(user -> user.getClass() == User.class && Objects.equals(user.getId(), "1234"))
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(userRepository, times(1)).findById(anyString());
     }
 }
